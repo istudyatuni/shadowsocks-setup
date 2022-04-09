232449b64ce5aee1aaa6dd2dba9a7af2729f014d
Ilia <istudyatuni@gmail.com>
2022-04-09 18:53:32 +0300

    feat: create directory for artifacts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process;
+use std::fs::create_dir_all;
 
 use clap::Command;
 use state::State;
@@ -11,7 +12,7 @@ fn main() {
     let matches =
         args::define_command_line_options(Command::new("Shadowsocks setup")).get_matches();
 
-    let sh = State::new(
+    let st = State::new(
         // this values are required
         matches.value_of("SERVER_PORT").unwrap(),
         matches.value_of("SERVER_PASSWORD").unwrap(),
@@ -22,14 +23,21 @@ fn main() {
         process::exit(1)
     });
 
-    let _: i32 = sh.server_port.parse().unwrap_or_else(|_| {
+    let _: i32 = st.server_port.parse().unwrap_or_else(|_| {
         eprintln!("Port shold be a number");
         process::exit(1);
     });
 
+    const ARTIFACTS_DIR: &str = "shadowsocks-artifacts";
+    create_dir_all(ARTIFACTS_DIR).unwrap_or_else(|e| {
+        eprintln!("Couldn't create directory: {e}");
+        process::exit(1);
+    });
+    st.sh.change_dir(ARTIFACTS_DIR);
+
     if let Some(install) = matches.value_of("INSTALL_TYPE") {
         match install {
-            "rust" => install::rust::run(&sh),
+            "rust" => install::rust::run(&st),
             "libev" => unimplemented!("libev not implemented"),
             _ => (),
         }
