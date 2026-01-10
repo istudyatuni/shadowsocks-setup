5d5565bf8baef7869044b2a4c382e101427d6232
Ilia <istudyatuni@gmail.com>
2026-01-10 19:14:23 +0300

    show response on github api failure

--- src/github.rs
+++ src/github.rs
@@ -5,14 +5,23 @@ use serde::Deserialize;
 pub fn get_latest_release_tag(owner: &str, repo: &str) -> Result<String> {
     let url = format!("https://api.github.com/repos/{owner}/{repo}/releases/latest");
 
-    let Release { tag_name } = Client::new()
+    let resp = Client::new()
         .get(url)
         .header("Accept", "application/vnd.github+json")
         .header("X-GitHub-Api-Version", "2022-11-28")
         .send()
         .context("failed to send request for latest github release")?
-        .json()
-        .context("failed to parse github response")?;
+        .error_for_status()?
+        .text()
+        .context("failed to get text of response")?;
+    let Release { tag_name } =
+        match serde_json::from_str::<Release>(&resp).context("failed to parse json") {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("github api returns invalid json:\n{resp}");
+                return Err(e);
+            }
+        };
 
     Ok(tag_name)
 }
